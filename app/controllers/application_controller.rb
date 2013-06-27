47f08b2e3f74e7bd434db94765a8ce99916f813d
gotqn <george_27@abv.bg>
2013-06-27 23:55:13 +0300

    Version 20130508

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  ##before_filter :is_profile_complete
+  #before_filter :is_profile_complete
 
   helper_method :current_user
 
